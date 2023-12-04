de0960978d77d24bb52100803852e981624a9865
Arun C Mohanan <aruncmunni@gmail.com>
2023-12-04 10:01:08 +0530

    updated cart checking

--- pkg/repository/cart.go
+++ pkg/repository/cart.go
@@ -165,7 +165,7 @@ func (cr *cartRepository) CheckCart(userID int) (bool, error) {
 	if err != nil {
 		return false, errors.New("no cart found")
 	}
-	if count < 0 {
+	if count < 1 {
 		return false, errors.New("no cart found")
 	}
 	return true, nil
