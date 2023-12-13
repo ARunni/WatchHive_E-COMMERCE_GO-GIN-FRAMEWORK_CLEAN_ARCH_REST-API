552bbe59d145f569cc9e4335b6be6d4a7e287e18
Arun C Mohanan <aruncmunni@gmail.com>
2023-12-13 15:14:04 +0530

    Updated payment table

--- pkg/domain/payment.go
+++ pkg/domain/payment.go
@@ -4,3 +4,11 @@ type PaymentMethod struct {
 	ID           uint   `json:"id" gorm:"primarykey;not null"`
 	Payment_Name string `json:"payment_name" gorm:"unique; not null"`
 }
+
+type Payment struct {
+	ID      uint   `json:"id" gorm:"primarykey not null"`
+	OrderId int    `json:"order_id"`
+	Order   Order  `json:"-" gorm:"foreignkey:OrderId"`
+	RazerID string `json:"razor_id"`
+	Payment string `json:"payment_id"`
+}
