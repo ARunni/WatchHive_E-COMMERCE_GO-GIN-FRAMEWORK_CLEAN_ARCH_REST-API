502299eea79d70baf7c0bce17bb12b020723d189
Arun C Mohanan <aruncmunni@gmail.com>
2024-01-04 13:25:09 +0530

    updated seagger for coupon

--- pkg/api/handler/coupon.go
+++ pkg/api/handler/coupon.go
@@ -18,6 +18,19 @@ func NewCouponHandler(coupon interfaces.CouponUsecase) *CouponHandler {
 	return &CouponHandler{CouponUsecase: coupon}
 }
 
+// AddCoupon Adding coupons
+// @Summary Add a new coupon
+// @Description Adds a new coupon based on the provided details.
+// @Tags Coupons
+// @Accept json
+// @Produce json
+// @Security BearerTokenAuth
+// @Param AddCoupon body models.Coupon true "Coupon details to add"
+// @Success 200 {object} response.Response "Success: Coupon added successfully"
+// @Failure 400 {object} response.Response "Bad request: Fields are provided in the wrong format"
+// @Failure 401 {object} response.Response "Unauthorized: Invalid or missing authentication"
+// @Failure 500 {object} response.Response "Internal server error: Could not add the coupon"
+// @Router /coupons [post]
 func (ch *CouponHandler) AddCoupon(c *gin.Context) {
 	var coupon models.Coupon
 	if err := c.BindJSON(&coupon); err != nil {
