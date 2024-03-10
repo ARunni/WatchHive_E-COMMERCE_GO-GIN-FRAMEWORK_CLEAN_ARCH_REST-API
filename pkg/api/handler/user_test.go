8466aba71821faa5cb5d8afc1052e5d363083316
Arun C Mohanan <aruncmunni@gmail.com>
2024-03-10 14:55:49 +0530

    user Test_User login handler  added

--- pkg/api/handler/user_test.go
+++ pkg/api/handler/user_test.go
@@ -95,3 +95,73 @@ func Test_UserSignUp(t *testing.T) {
 	}
 }
 
+func Test_LoginHandler(t *testing.T) {
+	testCase := map[string]struct {
+		input         models.UserLogin
+		buildstub     func(userCaseMock *mock_usecase.MockUserUseCase, login models.UserLogin)
+		checkResponse func(t *testing.T, responseRecorder *httptest.ResponseRecorder)
+	}{
+		"sucess": {
+			input: models.UserLogin{
+				Email:    "arun@gmail.com",
+				Password: "54321",
+			},
+			buildstub: func(userCaseMock *mock_usecase.MockUserUseCase, login models.UserLogin) {
+				err := validator.New().Struct(login)
+				if err != nil {
+					fmt.Println("validation failed")
+				}
+				userCaseMock.EXPECT().LoginHandler(login).Times(1).Return(models.TokenUsers{
+					Users: models.UserDetailsResponse{
+						Id:    1,
+						Name:  "arun",
+						Email: "arun@gmail.com",
+						Phone: "9747745981",
+					},
+					Token: "hdiuwhedui8wqey89.dikosaios",
+				}, nil)
+			},
+			checkResponse: func(t *testing.T, responseRecorder *httptest.ResponseRecorder) {
+				assert.Equal(t, http.StatusOK, responseRecorder.Code)
+			},
+		},
+		"user couldn't login": {
+			input: models.UserLogin{
+				Email:    "arun@gmail.com",
+				Password: "54321",
+			},
+			buildstub: func(userCaseMock *mock_usecase.MockUserUseCase, login models.UserLogin) {
+				err := validator.New().Struct(login)
+				if err != nil {
+					fmt.Println("validation failed")
+				}
+				userCaseMock.EXPECT().LoginHandler(login).Times(1).Return(models.TokenUsers{}, errors.New("cannot login up"))
+
+			},
+			checkResponse: func(t *testing.T, responseRecorder *httptest.ResponseRecorder) {
+				assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+			},
+		},
+	}
+
+	for testName, test := range testCase {
+		test := test
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+			ctrl := gomock.NewController(t)
+			mockUseCase := mock_usecase.NewMockUserUseCase(ctrl)
+			test.buildstub(mockUseCase, test.input)
+			UserHandler := NewUserHandler(mockUseCase)
+			server := gin.Default()
+			server.POST("/login", UserHandler.LoginHandler)
+			jsonData, err := json.Marshal(test.input)
+			assert.NoError(t, err)
+			body := bytes.NewBuffer(jsonData)
+			mockRequest, err := http.NewRequest(http.MethodPost, "/login", body)
+			assert.NoError(t, err)
+			responseRecorder := httptest.NewRecorder()
+			server.ServeHTTP(responseRecorder, mockRequest)
+			test.checkResponse(t, responseRecorder)
+		})
+	}
+}
