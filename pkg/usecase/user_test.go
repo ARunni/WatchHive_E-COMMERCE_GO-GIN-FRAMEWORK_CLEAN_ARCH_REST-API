d012aadf69a037d73ee433ea4e56a5c750d80c2a
Arun C Mohanan <aruncmunni@gmail.com>
2024-03-10 14:45:36 +0530

    user Test_GetUserDetails usecase  added

--- pkg/usecase/user_test.go
+++ pkg/usecase/user_test.go
@@ -54,3 +54,46 @@ func Test_GetAddress(t *testing.T) {
 		assert.Equal(t, test.wantErr, err)
 	}
 }
+
+func Test_GetUserDetails(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	userRepo := mockRepository.NewMockUserRepository(ctrl)
+	cfg := config.Config{}
+	wallet := mockRepository.NewMockWalletRepository(ctrl)
+	adminRepo := mockRepository.NewMockAdminRepository(ctrl)
+	helper := mockhelper.NewMockHelper(ctrl)
+
+	userUseCase := NewUserUseCase(userRepo, adminRepo, cfg, helper, wallet)
+	testingData := map[string]struct {
+		input   int
+		stub    func(*mockRepository.MockUserRepository, *mockhelper.MockHelper, int)
+		want    models.UsersProfileDetails
+		wantERr error
+	}{
+		"success": {
+			input: 1,
+			stub: func(mur *mockRepository.MockUserRepository, mh *mockhelper.MockHelper, i int) {
+				mur.EXPECT().ShowUserDetails(i).Times(1).Return(models.UsersProfileDetails{}, nil)
+			},
+			want:    models.UsersProfileDetails{},
+			wantERr: nil,
+		},
+		"failed": {
+			input: 1,
+			stub: func(mur *mockRepository.MockUserRepository, mh *mockhelper.MockHelper, i int) {
+				mur.EXPECT().ShowUserDetails(i).Times(1).Return(models.UsersProfileDetails{}, errors.New("error in getting details"))
+			},
+			want:    models.UsersProfileDetails{},
+			wantERr: errors.New("error in getting details"),
+		},
+	}
+	for _, test := range testingData {
+		test.stub(userRepo, helper, test.input)
+		result, err := userUseCase.ShowUserDetails(test.input)
+		assert.Equal(t, test.want, result)
+		assert.Equal(t, test.wantERr, err)
+	}
+
+}
